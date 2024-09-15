f48c78aba54e428f01c803ded438262ecf359d8d
SUSHMA NUKALA <sushmashivani@vt.edu>
2024-09-14 20:29:09 -0400

    updated project components s

--- src/SmartClassroomProject.tsx
+++ src/SmartClassroomProject.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const SmartClassroomProject: React.FC = () => {
   return (
     <div className="container project-container">
-      <Link to="/" className="btn btn-primary mt-3">Back to Portfolio</Link>
+  
 
       <h1 className="mt-5">Smart Classroom Project</h1>
 
